118b8d776391e35b40acc2fdf37b649d5bbe4ac0
Jin <tcofte7827@gmail.com>
2025-02-10 21:40:17 -0800

    feature/auth: updated to useForm

--- client/client/src/utils/api.js
+++ client/client/src/utils/api.js
@@ -1,36 +1,37 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'https://task-mgr-ozo2.onrender.com/api/v1', // Backend API URL
-    timeout: 10000, // Timeout after 10 seconds
-    headers: {
-        'Content-Type': 'application/json',
-    }
+  baseURL: "https://task-mgr-ozo2.onrender.com/api/v1", // Backend API URL
+  // baseURL: "http://localhost:5000/api/v1",
+  timeout: 10000, // Timeout after 10 seconds
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 // Intercept requests or responses (e.g., to add authorization headers)
 api.interceptors.request.use(
-    (config) => {
-        // Get the token from localStorage or sessionStorage
-        const token = localStorage.getItem('token') || sessionStorage.getItem('token');
+  (config) => {
+    // Get the token from localStorage or sessionStorage
+    const token =
+      localStorage.getItem("token") || sessionStorage.getItem("token");
 
-        // If the token exists, add it to the Authorization header
-        if (token) {
-            config.headers.Authorization = `Bearer ${token}`;
-        }
-
-        return config;
-    },
-    (error) => Promise.reject(error) // Handle request errors
+    // If the token exists, add it to the Authorization header
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error) // Handle request errors
 );
 
 // Handling response errors globally
 api.interceptors.response.use(
-    (response) => response,
-    (error) => {
-        console.error('API error:', error.response || error.message);
-        return Promise.reject(error);
-    }
+  (response) => response,
+  (error) => {
+    console.error("API error:", error.response || error.message);
+    return Promise.reject(error);
+  }
 );
 
 export default api;
